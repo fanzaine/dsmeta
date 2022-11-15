9ef5acb5935550d22916438c590297b07b56f2d2
fanzaine <danielsantos46@live.com>
2022-11-15 19:33:22 -0300

    dsmeta

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import Notificationbutton from "./components/Notificationbutton"
+
+function App() {
+return(
+  <>
+  <h1> ola mundo</h1>
+  <Notificationbutton />
+  </>
+)
+}
+
+export default App
