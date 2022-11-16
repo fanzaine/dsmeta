d879da88bb6f7dd768492ba4631c5249489ab938
fanzaine <danielsantos46@live.com>
2022-11-15 21:04:13 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 
+import Header from "./components/Header"
 import Notificationbutton from "./components/Notificationbutton"
+import SalesCard from "./components/SaleCard"
 
 function App() {
 return(
   <>
-  <h1> ola mundo</h1>
-  <Notificationbutton />
-  <Notificationbutton />
+   <Header />
+   <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard />
+          </div>
+          </section>
+          </main>
+
+
   </>
 )
 }
